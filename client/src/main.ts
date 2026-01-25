b4ac16819e196d0b8a4044dfde4b6ff7767abdb0
muhammadilyasafadhlih <muhammadilyasafadhlih@gmail.com>
2026-01-25 06:09:28 +0000

    Add base url to vue router

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,7 +7,7 @@ import Landing from "./ui/pages/Landing.vue";
 import MainMenu from "./ui/pages/MainMenu.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
